502a0842a27ce3a5ed282265a4eea19b2fd86365
Mihai <mihai.ocneanu@gmail.com>
2019-11-26 11:48:04 +0200

    refactor, cleanup & more

--- src/resources/js/icons.js
+++
@@ -1,9 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-// import { } from '@fortawesome/free-regular-svg-icons';
-
-import { faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons';
-
-// import { } from '@fortawesome/free-brands-svg-icons';
-
-library.add(faHandHoldingUsd);
