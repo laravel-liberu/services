eee8ce46349adfd51aed2c16305a53bf166da0be
reza <raftx24@gmail.com>
2019-12-23 22:45:24 +0330

    upgrades to php7.4

--- src/database/factories/ServiceFactory.php
+++ src/database/factories/ServiceFactory.php
@@ -4,19 +4,15 @@ use Faker\Generator as Faker;
 use LaravelEnso\MeasurementUnits\app\Models\MeasurementUnit;
 use LaravelEnso\Services\app\Models\Service;
 
-$factory->define(Service::class, function (Faker $faker) {
-    return [
-        'measurement_unit_id' => function () {
-            return (factory(MeasurementUnit::class)->create())->id;
-        },
-        'name' => $faker->name,
-        'code' => $faker->unique()->ean8,
-        'list_price' => $faker->numberBetween(1, 300),
-        'vat_percent' => collect([5, 19, 24])->random(),
-        'description' => $faker->text,
-        'is_active' => true,
-    ];
-});
+$factory->define(Service::class, fn(Faker $faker) => [
+    'measurement_unit_id' => fn() => factory(MeasurementUnit::class)->create()->id,
+    'name' => $faker->name,
+    'code' => $faker->unique()->ean8,
+    'list_price' => $faker->numberBetween(1, 300),
+    'vat_percent' => collect([5, 19, 24])->random(),
+    'description' => $faker->text,
+    'is_active' => true,
+]);
 
 $factory->afterMaking(Service::class, function ($invoice, $faker) {
     $invoice->inCents = true;
