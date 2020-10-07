e0b68b9f9261e0e1ba0e58cd2b6560397584b2cc
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:43:13 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Services\Http\Controllers\Create;
+use LaravelEnso\Services\Http\Controllers\Destroy;
+use LaravelEnso\Services\Http\Controllers\Edit;
+use LaravelEnso\Services\Http\Controllers\ExportExcel;
+use LaravelEnso\Services\Http\Controllers\InitTable;
+use LaravelEnso\Services\Http\Controllers\Options;
+use LaravelEnso\Services\Http\Controllers\Store;
+use LaravelEnso\Services\Http\Controllers\TableData;
+use LaravelEnso\Services\Http\Controllers\Update;
 
 Route::middleware(['api', 'auth', 'core'])
-    ->namespace('LaravelEnso\Services\Http\Controllers')
     ->prefix('api/services')
     ->as('services.')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{service}/edit', 'Edit')->name('edit');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{service}/edit', Edit::class)->name('edit');
 
-        Route::patch('{service}', 'Update')->name('update');
+        Route::patch('{service}', Update::class)->name('update');
 
-        Route::delete('{service}', 'Destroy')->name('destroy');
+        Route::delete('{service}', Destroy::class)->name('destroy');
 
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
+        Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
 
-        Route::get('options', 'Options')->name('options');
+        Route::get('options', Options::class)->name('options');
     });
