ccaa353680e6da0a111da86dc59e0715aef018ed
mishu <mishu@xtelecom.ro>
2021-06-03 10:08:48 +0300

    improves upgrade ;

--- src/Upgrades/MeasurementUnitNotNullable.php
+++ src/Upgrades/MeasurementUnitNotNullable.php
@@ -3,18 +3,12 @@
 namespace LaravelEnso\Services\Upgrades;
 
 use Illuminate\Support\Facades\Schema;
-use LaravelEnso\Upgrade\Contracts\Applicable;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
 use LaravelEnso\Upgrade\Helpers\Column;
 
 class MeasurementUnitNotNullable implements MigratesTable
 {
     public function isMigrated(): bool
-    {
-        return false;
-    }
-
-    public function applicable(): bool
     {
         return Column::isNotNullable('services', 'measurement_unit_id');
     }
