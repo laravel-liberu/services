e214422d1a79e86a1ef04275931b61783d806e9c
Mihai <mihai.ocneanu@gmail.com>
2021-06-09 18:04:42 +0300

    fixed exception on null / service creation

--- src/Http/Requests/ValidateServiceRequest.php
+++ src/Http/Requests/ValidateServiceRequest.php
@@ -28,6 +28,6 @@ class ValidateServiceRequest extends FormRequest
     private function uniqueCode()
     {
         return Rule::unique('services', 'code')
-            ->ignore($this->route('service')->id);
+            ->ignore($this->route('service')?->id);
     }
 }
